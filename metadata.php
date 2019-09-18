4facdd9ad6c4e95d1b98c1764d2bf3e91bf1e1c1
Robert Rosendahl <rr@digitalguru.de>
2019-09-18 02:25:51 +0200

    Version 2.0.2

--- metadata.php
+++ metadata.php
@@ -29,7 +29,7 @@ $sMetadataVersion = '2.0';
 
 $aModule = array(
     'id' => 'ghoxid2greyhoundconnect',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'title' => array(
         'en' => 'GREYHOUND OXID Connect Module',
         'de' => 'GREYHOUND OXID Connect Modul',
